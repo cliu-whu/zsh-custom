960e68642748e1d0e538276b0d2771bef5482849
cliu-whu <liucongns@whu.edu.cn>
2022-01-10 20:37:04 +0800

    1

--- lib/plugins.sh
+++ lib/plugins.sh
@@ -1,4 +1,4 @@
 ZSH_CUSTOM='/opt/zsh-custom'
 source ${ZSH_CUSTOM}/zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
 source ${ZSH_CUSTOM}/zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh
-source ${ZSH_CUSTOM}/z/z.plugin.zsh
+source ${ZSH_CUSTOM}/autojump/autojump.plugin.zsh
