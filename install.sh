4d4da1bf6a479582fa85d9b093fbf1d99de4f8ea
cliu-whu <liucongns@whu.edu.cn>
2022-01-10 20:23:45 +0800

    test

---
+++ install.sh
@@ -0,0 +1,4 @@
+ZSH_CUSTOM='/opt/zsh-custom'
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/zsh-autosuggestions
+git clone https://github.com/wting/autojump.git $ZSH_CUSTOM/autojump
---
+++ zsh-custom.sh
@@ -0,0 +1,3 @@
+export ZSH_CUSTOM='/opt/zsh-custom'
+source ${ZSH_CUSTOM}/lib/plugins.sh
+source ${ZSH_CUSTOM}/lib/theme.sh
